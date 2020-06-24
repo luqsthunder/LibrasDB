42ca16fb760e12c1f25eb09220a9255b2166e2b6
Lucas <lafa@ic.ufal.br>
2020-06-24 19:34:17 -0300

    Modificações para remoção de duplicatas e checagem de EAFs divergentes.

--- db/all_eaf_parse_2_csv.py
+++ db/all_eaf_parse_2_csv.py
@@ -17,11 +17,12 @@ class AllEAFParser2CSV:
         videos e legendas respectivos de cada um.
         """
         # nome de todas as pastas presentes na base de dados.
-        res = os.listdir('.')
         self.estates_path_in_db = [os.path.join(base_db_path, x)
                                    for x in os.listdir(base_db_path)]
         self.estates_path_in_db = list(filter(lambda x: os.path.isdir(x),
                                               self.estates_path_in_db))
+        self.bad_videos = []
+        self.bad_subs = []
 
     def process(self):
         """
@@ -33,10 +34,11 @@ class AllEAFParser2CSV:
         """
         gen_subs = self.__gen_xmls_base_subs()
         amount_folders = next(gen_subs)
-        libras_df = pd.DataFrame(columns=['sign', 'beg', 'end', 'folder_name',
-                                          'talker_id', 'hand'])
+        df_cols = ['sign', 'beg', 'end', 'folder_name', 'talker_id', 'hand']
+        libras_df = pd.DataFrame(columns=df_cols)
 
-        for subs_xml, videos in tqdm(gen_subs, total=amount_folders):
+        for subs_xml, videos in tqdm(gen_subs, total=amount_folders,
+                                     desc='Processing EAFs'):
             # TODO: destrinxar as legendas e comparar se elas são iguais
 
             if len(subs_xml) == 0 or len(videos) == 0:
@@ -46,27 +48,99 @@ class AllEAFParser2CSV:
             subs = [self.__get_subs(x) for x in subs_xml]
 
             if len(subs) > 0:
+                has_subs_diff = False
                 l_s = subs[0]
                 for s in subs[1:]:
                     if not l_s.equals(s):
                         print('found subs with differences in ', videos[0])
+                        has_subs_diff = True
 
-            signs = [x[1]['sub'] for x in subs[0].iterrows()]
-            begs = [x[1]['beg'] for x in subs[0].iterrows()]
-            ends = [x[1]['end'] for x in subs[0].iterrows()]
+                if has_subs_diff:
+                    for it in range(len(subs)):
+                        dif_df = pd.DataFrame(columns=df_cols)
+                        name_df = f'{videos[it]}-diff_df-it-{it}.csv'
+                        self.__update_sign_df(dif_df, subs[it],
+                                              time_stamps[it],
+                                              videos[0]).to_csv(name_df)
+                    continue
 
-            begs = list(map(lambda x: time_stamps[0][x], begs))
-            ends = list(map(lambda x: time_stamps[0][x], ends))
+            # iterrows retorna uma tupla com (idx: int, row: pd.Series)
+            libras_df = self.__update_sign_df(libras_df, subs[0], time_stamps[0],
+                                              videos[0])
 
-            talker_id = [x[1]['talker_id'] for x in subs[0].iterrows()]
-            hand = [x[1]['hand'] for x in subs[0].iterrows()]
-            video = [videos[0]] * len(signs)
+        # Nos arquivos EAF do Corpus de Libras cada falante quando executa um
+        # sinal com ambas as mãos é colocado em duplicidade no EAF.
+        # Nessa etapa abaixo removemos as duplicidades pois não é interessante
+        # saber ja que vamos extrair o esqueleto posteriormente.
+        # row_2_drop = []
+        # for it, row in tqdm(enumerate(libras_df.iterrows()), desc='dups'):
+        #     row = row[1]
+        #     res = libras_df.loc[(libras_df['beg'] == row['beg']) &
+        #                         (libras_df['end'] == row['end']) &
+        #                         (libras_df['talker_id'] == row['talker_id']) &
+        #                         (libras_df['folder_name'] == row['folder_name']) &
+        #                         (libras_df['sign'] == row['sign'])]
+        #     if res.shape[0] > 1:
+        #         libras_df.loc[it, 'hand'] = 2
+        #         row_2_drop.append(res.index)
+        #     else:
+        #         libras_df.loc[it, 'hand'] = 1
+        # single_list_drop = list(map(lambda x: x[1], row_2_drop))
+        # single_list_drop = list(set(single_list_drop))
+        # libras_df = libras_df.drop(single_list_drop)
+        libras_df.to_csv('dupl-all_videos.csv')
 
-            data_dict = dict(sign=signs, beg=begs, end=ends, folder_name=video,
-                             talker_id=talker_id, hand=hand)
-            libras_df = libras_df.append(pd.DataFrame(data=data_dict),
-                                         ignore_index=True)
-        libras_df.to_csv('all_videos.csv')
+    @staticmethod
+    def __update_sign_df(df: pd.DataFrame, subs: pd.DataFrame,
+                         time_stamps: dict, video: str):
+        """
+        Atualiza o dat;,aframe com as seguintes colunas:
+        sinais: str nome do sinal;
+        beg: int (inicio em milisegundo do sinal no video de acordo com a
+        legenda);
+        end: int (final em milisegundo do sinal no video de acordo com a
+        legenda);
+        folder_name: str nome da pasta / video;
+        talker_id: int ID da de quem sinaliza de acordo com a legenda;
+        hand: int quantas mãos são utilizadas na execução do sinal.
+
+        Parameters
+        ----------
+        df: pd.DataFrame
+            DataFrame a ser atualizado. Que deve/irá conter os sinais.
+
+        subs:
+            Legendas/Nome dos sinais.
+
+        time_stamps: dict
+            dicionario de timestamps. No formato { 'tsX': int_value }, com 'tsX'
+            indicando a chave no tempo e int_value o valor de tempo em
+            milisegundos.
+
+        video: str
+            Nome do video a quem as legendas pertencem.
+
+        Returns
+        -------
+        df: pd.DataFrame
+            DataFrame atualizado com as novas entradas.
+
+        """
+        signs = [x[1]['sub'] for x in subs.iterrows()]
+        begs = [x[1]['beg'] for x in subs.iterrows()]
+        ends = [x[1]['end'] for x in subs.iterrows()]
+
+        begs = list(map(lambda x: time_stamps[x], begs))
+        ends = list(map(lambda x: time_stamps[x], ends))
+
+        talker_id = [x[1]['talker_id'] for x in subs.iterrows()]
+        hand = [x[1]['hand'] for x in subs.iterrows()]
+        video = [video] * len(signs)
+
+        data_dict = dict(sign=signs, beg=begs, end=ends, folder_name=video,
+                         talker_id=talker_id, hand=hand)
+
+        return df.append(pd.DataFrame(data=data_dict), ignore_index=True)
 
     @staticmethod
     def __get_subs(tree):
@@ -122,20 +196,6 @@ class AllEAFParser2CSV:
                                                         talker_id=talkers_id,
                                                         hand=hands)),
                                  ignore_index=True)
-        indexes_2_drop = []
-        for row in signs.iterrows():
-            # vem como uma tupla (idx, Series)
-            row = row[1]
-
-            ret = signs.loc[(signs['beg'] == row['beg']) &
-                            (signs['end'] == row['end']) &
-                            (signs['sub'] == row['sub']) &
-                            (signs['talker_id'] == row['talker_id'])]
-            print(ret.shape)
-            if ret.shape[0] > 1:
-                signs.loc[ret['hand'].index, 'hand'] = 2
-                indexes_2_drop.append(ret.index[1])
-
         return signs
 
 
@@ -146,8 +206,8 @@ class AllEAFParser2CSV:
 
         Parameters
         ----------
-
         tree : xml.etree.ElementTree
+            arvore contendo o XML/EAF da legenda no video.
 
         Returns
         -------
@@ -182,7 +242,7 @@ class AllEAFParser2CSV:
         """
         Funcao para criar os xpaths e iterar com generator sobre toda a base.
 
-        Returns
+        Yields
         -------
 
         Generator com cada video e legenda dentro da base.
@@ -214,7 +274,7 @@ class AllEAFParser2CSV:
                         list(map(lambda x: os.path.join(item_path, x),
                                  items_dir))
 
-                    subs = list(filter(lambda x: '.xml' in x, items_dir))
+                    subs = list(filter(lambda x: '.EAF' in x, items_dir))
                     subs_parsed = []
                     failed_subs_count = 0
                     for s in subs:
@@ -223,37 +283,42 @@ class AllEAFParser2CSV:
                             subs_parsed.append(s)
                         except et.ParseError as e:
                             failed_subs_count += 1
+                            self.bad_subs.append((s, item_path))
 
                     if failed_subs_count == len(subs):
                         yield [], []
                         continue
 
                     videos = list(filter(lambda x: '.mp4' in x, items_dir))
-                    failed_video_count = 0
+                    failed_video = [False, False, False, False]
                     good_videos = []
-                    for v in videos:
+                    for it, v in enumerate(videos):
                         try:
                             vid = cv.VideoCapture(v)
                             ret, frame = vid.read()
                             if not vid.isOpened() or (not ret):
-                                failed_video_count += 1
+                                failed_video[it] = True
                                 vid.release()
                                 continue
                         except cv.error:
-                            failed_video_count += 1
+                            failed_video[it] = True
                         else:
                             good_videos.append(v)
                             vid.release()
-
-                    if len(good_videos) == 0 or len(subs_parsed) == 0:
+                    if failed_video[0] and failed_video[3]:
+                        self.bad_videos.append((videos, item_path))
                         yield [], []
+                        continue
+
+                    # if len(good_videos) == 0 or len(subs_parsed) == 0:
+                    #     yield [], []
+                    #     continue
 
                     yield subs_parsed, good_videos
 
-    def parse_time_slots(tree):
-        pass
-
 
 if __name__ == '__main__':
-    db_cut_videos = AllEAFParser2CSV('./db')
+    db_cut_videos = AllEAFParser2CSV('../LibrasCorpusScrapy/db')
     db_cut_videos.process()
+    print(db_cut_videos.bad_subs, file=open('bad_subs.txt', mode='w'))
+    print(db_cut_videos.bad_videos, file=open('bad_videos.txt', mode='w'))
