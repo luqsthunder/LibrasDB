3ca2c714bd2fff00beda2a307ff50807141c5900
Lucas <lafa@ic.ufal.br>
2020-06-24 19:33:46 -0300

    Codigo usado no colab para extração de poses.

--- colab_tests.py
+++ colab_tests.py
@@ -17,7 +17,6 @@ import pickle
 from matplotlib.backends.backend_pdf import PdfPages
 
 
-
 def convert_xypose_to_dir_angle(first_pose, second_pose, third_pose):
     """
     Parameters
@@ -54,12 +53,15 @@ def make_xy_centroid(body_pose, pose_acc_min_threshold=0.2):
     y_points = [x[1] for x in body_pose
                 if x[2] > pose_acc_min_threshold and x[0] > 1 and x[1] > 1]
 
+    if len(x_points) == 0:
+        return np.array([])
+
     return np.array([sum(x_points) / len(x_points),
                      sum(y_points) / len(y_points)])
 
 
 def track_by_centroid(curr_persons_list, person_centroid,
-                      ret_new_centroid=False):
+                      ret_new_centroid=False, ret_dist=False):
     """
     as listas devem conter um dicionario nesse formato: {
         id: int
@@ -76,12 +78,20 @@ def track_by_centroid(curr_persons_list, person_centroid,
     new_centroid = None
 
     for c_person in curr_persons_list:
+        if c_person['centroid'].size == 0:
+            continue
+
         centroids_dist = \
             np.linalg.norm(person_centroid - c_person['centroid'])
         if centroids_dist < curr_person_dist:
             curr_person_dist = centroids_dist
             correct_id = c_person['id']
             new_centroid = c_person['centroid']
+
+    if ret_dist and ret_new_centroid:
+        return correct_id, new_centroid, curr_person_dist
+    if ret_dist and not ret_new_centroid:
+        return correct_id, curr_person_dist
 
     return correct_id if not ret_new_centroid else correct_id, new_centroid
 
@@ -98,9 +108,13 @@ def make_hands_list(dt):
     hands_list = []
     # isso tem cara de ser 2 mão x N pessoas
     for it, persons in enumerate(dt.handKeypoints[0]):
+        centroid = make_xy_centroid(dt.handKeypoints[0][it])
+
         hand = {
             'id': it,
-            'centroid': make_xy_centroid(dt.handKeypoints[0][it])}
+            'centroid': centroid,
+            'failed': False
+        }
         hands_list.append(hand)
     return hands_list
 
@@ -118,7 +132,9 @@ def make_persons_list(dt):
     for it, persons in enumerate(dt.poseKeypoints):
         person = {
             'id': it,
-            'centroid': make_xy_centroid(dt.poseKeypoints[it])}
+            'centroid': make_xy_centroid(dt.poseKeypoints[it]),
+            'failed': False
+        }
         persons_list.append(person)
     return persons_list
 
@@ -142,12 +158,17 @@ def calculate_angle_by_joints(dt, person, first_joint_id, second_joint_id,
     return angle
 
 
-def update_anglepose_df(df, dt, angles_list, headers):
+def update_anglepose_df(df, angles_list, headers, person_id, frame):
     pose_dict = dict(zip(headers, angles_list))
-    return df.append(pose_dict)
+    pose_dict.update({
+        'person': person_id,
+        'frame': frame
+    })
+    return df.append(pose_dict, ignore_index=True)
 
 
 def update_xypose_df(df, datum, person, body_joints,
+                     person_id, frame,
                      hand_joints=None, head_joints=None,
                      use_acc_treshold=False, acc_treshold=0.3):
     """
@@ -159,32 +180,54 @@ def update_xypose_df(df, datum, person, body_joints,
     """
 
     pose_dic = {
-        c: datum.poseKeypoints[person, BODY_PARTS[c]][: 2]
-        for it, c in enumerate(body_joints)
-        if (not use_acc_treshold) or
-           datum.poseKeypoints[person, BODY_PARTS[c]][2] > acc_treshold
+        'person': person_id,
+        'frame': frame
     }
+    body_parts_dict = {}
+    for c in body_joints:
+        try:
+            if (not use_acc_treshold) or \
+                    datum.poseKeypoints[person, BODY_PARTS[c]][
+                        2] > acc_treshold:
+                body_parts_dict.update(
+                    {c: datum.poseKeypoints[person, BODY_PARTS[c]][: 2]})
+            else:
+                body_parts_dict.update({c: None})
+        except IndexError:
+            body_parts_dict.update({c: None})
+
+    pose_dic.update(body_parts_dict)
     if hand_joints is not None:
         pose_dic.update({
-            (c + 'r'): datum.handKeypoints[0][person, HAND_PARTS[c]][: 2]
-            for it, c in enumerate(hand_joints)
-            if (not use_acc_treshold) or
-               datum.handKeypoints[0][person, HAND_PARTS[c]][2] > acc_treshold
+            (c + 'r'): datum.handKeypoints[0][person, HAND_PARTS[c]][: 2] if (
+                                                                                 not use_acc_treshold) or
+                                                                             datum.handKeypoints[
+                                                                                 0][
+                                                                                 person,
+                                                                                 HAND_PARTS[
+                                                                                     c]][
+                                                                                 2] > acc_treshold else None
+            for c in hand_joints
         })
 
         pose_dic.update({
-            (c + 'l'): datum.handKeypoints[1][person, HAND_PARTS[c]][: 2]
-            for it, c in enumerate(hand_joints)
-            if (not use_acc_treshold) or
-               datum.handKeypoints[1][person, HAND_PARTS[c]][2] > acc_treshold
+            (c + 'l'): datum.handKeypoints[1][person, HAND_PARTS[c]][: 2] if (
+                                                                                 not use_acc_treshold) or
+                                                                             datum.handKeypoints[
+                                                                                 1][
+                                                                                 person,
+                                                                                 HAND_PARTS[
+                                                                                     c]][
+                                                                                 2] > acc_treshold else None
+            for c in hand_joints
         })
 
     # if head_joints is not None:
 
-    return df.append(pose_dic, ignore_index=False)
+    return df.append(pose_dic, ignore_index=True)
 
 
-def update_r_hands_list(dt, list_persons):
+def update_r_hands_list(dt, list_persons, dist_threshold=120):
     """
     Parameters
     ----------
@@ -197,18 +240,26 @@ def update_r_hands_list(dt, list_persons):
 
     r_hands = []
     for old_person in list_persons:
-        correct_id, centroid = track_by_centroid(curr_r_hands_list,
-                                                 old_person['centroid'],
-                                                 ret_new_centroid=True)
+        failed_to_track = False
+        correct_id, centroid, dist = track_by_centroid(curr_r_hands_list,
+                                                       old_person['centroid'],
+                                                       ret_new_centroid=True,
+                                                       ret_dist=True)
+        if dist > dist_threshold:
+            centroid = old_person['centroid']
+            correct_id = old_person['id']
+            failed_to_track = True
+
         r_hands.append({
             'id': correct_id,
-            'centroid': centroid
+            'centroid': centroid,
+            'failed': failed_to_track
         })
 
     return r_hands
 
 
-def update_person_list(dt, old_list_persons):
+def update_person_list(dt, old_list_persons, dist_threshold=30):
     """
     Atualiza a lista de pessoas baseado no centroid anterior e atual.
 
@@ -234,12 +285,20 @@ def update_person_list(dt, old_list_persons):
 
     persons = []
     for old_person in old_list_persons:
-        correct_id, centroid = track_by_centroid(curr_person_list,
-                                                 old_person['centroid'],
-                                                 ret_new_centroid=True)
+        failed_to_track = False
+        correct_id, centroid, dist = track_by_centroid(curr_person_list,
+                                                       old_person['centroid'],
+                                                       ret_new_centroid=True,
+                                                       ret_dist=True)
+        if dist > dist_threshold:
+            centroid = old_person['centroid']
+            correct_id = old_person['id']
+            failed_to_track = True
+
         persons.append({
             'id': correct_id,
-            'centroid': centroid
+            'centroid': centroid,
+            'failed': failed_to_track
         })
 
     return persons
@@ -271,17 +330,19 @@ opWrapper.start()
 person_last_list = None
 right_hands_last_list = None
 
-db_path = './drive/My Drive/librasdb-pose-window-500ms/'
+db_path = './drive/My Drive/librasdb-videos-window-500ms/'
+acc_threshold = 0.2
 
 for sign_dir in tqdm(os.listdir(db_path)):
-
     sign_name = copy.copy(sign_dir)
     sign_dir = os.path.join(db_path, sign_dir)
-
-    for file_path in os.listdir(sign_dir):
-
+    for file_path in tqdm(os.listdir(sign_dir),
+                          desc='Sign {}'.format(sign_name)):
         file_name = copy.copy(file_path)
         file_path = os.path.join(sign_dir, file_path)
+
+        if not os.path.isfile(file_path):
+            continue
 
         anglepose_df = pd.DataFrame(columns=df_cols_angles)
         xypose_df = pd.DataFrame(columns=df_cols_xy)
@@ -293,7 +354,7 @@ for sign_dir in tqdm(os.listdir(db_path)):
             ret, frame = single_video_ref.read()
             frame_pos = single_video_ref.get(cv.CAP_PROP_POS_FRAMES)
             if not ret:
-                video_finished_correctly = frame_pos == (last_frame_pos - 1)
+                video_finished_correctly = frame_pos == last_frame_pos
                 break
 
             datum = op.Datum()
@@ -311,17 +372,6 @@ for sign_dir in tqdm(os.listdir(db_path)):
 
                 for it, person_tracked in enumerate(person_last_list):
                     person = person_tracked['id']
-                    if it == 0:
-                        centroid = person_tracked['centroid']
-                        cv.circle(imageToProcess, tuple(centroid), 5,
-                                  (128, 0, 128), -1)
-                        r_hand_id = r_hands_list[0]['id']
-                        centroid = r_hands_list[0]['centroid']
-                        cv.circle(imageToProcess, tuple(centroid), 5,
-                                  (128, 0, 128), -1)
-                        centroid = make_xy_centroid(datum.handKeypoints[1][r_hand_id])
-                        cv.circle(imageToProcess, tuple(centroid), 5,
-                                  (128, 0, 128), -1)
                     angles_pose = []
 
                     for it, pair in enumerate(POSE_PAIRS[:7]):
@@ -329,35 +379,52 @@ for sign_dir in tqdm(os.listdir(db_path)):
                         second = BODY_PARTS[pair[0]]
                         third = BODY_PARTS[pair[1]]
 
-                        # checagem se tem pouca acuracia.
+                        # TODO:
+                        # -  adicionar Nan as posições com baixa acuracia e a
+                        #    toda linha caso não tenha achado o esqueledo
+                        try:
+                            angle = calculate_angle_by_joints(datum, person,
+                                                              first, second,
+                                                              third)
+                        except IndexError:
+                            angles_pose.append(None)
+                            continue
 
-                        angle = calculate_angle_by_joints(datum, person, first,
-                                                          second, third)
-                        second = datum.poseKeypoints[person, second][:2]
-                        third = datum.poseKeypoints[person, third][:2]
+                        first = datum.poseKeypoints[person, first]
+                        second = datum.poseKeypoints[person, second]
+                        third = datum.poseKeypoints[person, third]
 
-                        angles_pose.append(angle)
+                        if first[2] > acc_threshold and \
+                                second[2] > acc_threshold and \
+                                third[2] > acc_threshold and \
+                                person_tracked['failed']:
+                            angles_pose.append(angle)
+                        else:
+                            angles_pose.append(None)
 
-                    xypose_df = update_xypose_df(xypose_df, datum, person,
-                                                 df_cols_xy)
-                    anglepose_df = update_anglepose_df(anglepose_df, datum,
+                    if not person_tracked['failed']:
+                        xypose_df = update_xypose_df(xypose_df, datum, person,
+                                                     df_cols_xy[2:],
+                                                     person_tracked['id'],
+                                                     frame_pos)
+
+                    anglepose_df = update_anglepose_df(anglepose_df,
                                                        angles_pose,
-                                                       df_cols_angles)
-
-            plt.imshow(imageToProcess)
-            plt.show()
+                                                       df_cols_angles[2:],
+                                                       person_tracked['id'],
+                                                       frame_pos)
 
         if not video_finished_correctly:
             print(last_frame_pos, frame_pos, file_path)
         else:
             file_name = file_name.split('.')[0] + '.csv'
-            print(file_name)
-            # if not os.path.exists(db_path + sign_name + '/xy/'):
-            #     os.makedirs(db_path + sign_name + '/xy/')
-            # xypose_df.to_csv(db_path + sign_name + '/xy/' + file_name)
-            #
-            # if not os.path.exists(db_path + sign_name + '/angle/'):
-            #     os.makedirs(db_path + sign_name + '/angle/')
-            # anglepose_df.to_csv(db_path + sign_name + '/angle/' + file_name)
+            if not os.path.exists(db_path + sign_name + '/no_hands-xy/'):
+                os.makedirs(db_path + sign_name + '/no_hands-xy/')
+            xypose_df.to_csv(db_path + sign_name + '/no_hands-xy/' + file_name)
+
+            if not os.path.exists(db_path + sign_name + '/no_hands-angle/'):
+                os.makedirs(db_path + sign_name + '/no_hands-angle/')
+            anglepose_df.to_csv(
+                db_path + sign_name + '/no_hands-angle/' + file_name)
 
         single_video_ref.release()
