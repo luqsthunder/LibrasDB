d97b55027c361ff46f585e6389fc1600e6057a13
luqsthunder <lucasthund3r@gmail.com>
2020-08-05 15:46:53 -0300

    Removendo indices quando salva CSV da base. Visualizador dos videos e samples. Upando codigo do extrator de centroids do colab

--- db/all_eaf_parse_2_csv.py
+++ db/all_eaf_parse_2_csv.py
@@ -154,7 +154,7 @@ class AllEAFParser2CSV:
         row_2_drop = []
         path_2_dup_all_videos = os.path.join(path_to_save_sign_df,
                                              'dupl-all_videos.csv')
-        libras_df.to_csv(path_2_dup_all_videos)
+        libras_df.to_csv(path_2_dup_all_videos, index=False)
         pbar_dup = tqdm(total=libras_df.shape[0], desc='dups') \
             if pbar_dup is None else pbar_dup
 
@@ -180,11 +180,11 @@ class AllEAFParser2CSV:
         libras_df = libras_df.drop(single_list_drop)
 
         path_2_all_videos = os.path.join(path_to_save_sign_df, 'all_videos.csv')
-        libras_df.to_csv(path_2_all_videos)
+        libras_df.to_csv(path_2_all_videos, index=False)
 
     def remove_db_df_path_specific(self, df):
         """
-        
+
         Parameters
         ----------
         df
--- visualization/db_player.py
+++ visualization/db_player.py
@@ -1,6 +1,8 @@
 import cv2
+import os
 import sys
 import pandas as pd
+import time
 from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QPushButton, \
@@ -22,6 +24,12 @@ class OCVVideoThread(QThread):
         pass
 
     def next_vid(self):
+        pass
+
+    def prev_sample(self):
+        pass
+
+    def next_sample(self):
         pass
 
     def run(self):
@@ -49,9 +57,15 @@ class App(QWidget):
     pause_btn = None
     video_name_label = None
     th = None
+    th_cls = None
+    th_cls_kwargs = None
+    next_sample_btn = None
+    prev_sample_btn = None
 
-    def __init__(self):
+    def __init__(self, th_cls=OCVVideoThread, **th_cls_kwargs):
         super().__init__()
+        self.th_cls=th_cls
+        self.th_cls_kwargs=th_cls_kwargs
         self.init_ui()
 
     @pyqtSlot(QImage)
@@ -61,18 +75,24 @@ class App(QWidget):
     def init_ui(self):
         self.setWindowTitle(self.title)
         # self.setGeometry(self.left, self.top, self.width, self.height)
-        self.next_btn = QPushButton('next')
-        self.prev_btn = QPushButton('prev')
+        self.next_btn = QPushButton('next video')
+        self.prev_btn = QPushButton('prev video')
+        self.next_sample_btn = QPushButton('next sample')
+        self.prev_sample_btn = QPushButton('prev sample')
         self.pause_btn = QPushButton('pause')
 
         self.pause_btn.clicked.connect(self.click_pause_btn)
-        # self.prev_btn.clicked.connect(self.click_prev_btn)
-        # self.next_btn.clicked.connect(self.click_next_btn)
+        self.prev_btn.clicked.connect(self.click_prev_btn)
+        self.next_btn.clicked.connect(self.click_next_btn)
+        self.prev_sample_btn.clicked.connect(self.click_prev_sample_btn)
+        self.next_sample_btn.clicked.connect(self.click_next_sample_btn)
 
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(self.prev_btn)
+        hbox.addWidget(self.prev_sample_btn)
         hbox.addWidget(self.pause_btn)
+        hbox.addWidget(self.next_sample_btn)
         hbox.addWidget(self.next_btn)
 
         vbox = QVBoxLayout()
@@ -83,7 +103,7 @@ class App(QWidget):
         self.label = QLabel(self)
         self.label.resize(1366, 768)
 
-        self.th = OCVVideoThread(self)
+        self.th = self.th_cls(**self.th_cls_kwargs)
 
         vbox.addWidget(self.label)
 
@@ -98,26 +118,119 @@ class App(QWidget):
     def click_pause_btn(self):
         self.th.pause = not self.th.pause
 
+    def click_next_sample_btn(self):
+        self.th.next_sample()
+
+    def click_prev_sample_btn(self):
+        self.th.prev_sample()
+
+    def click_next_btn(self):
+        self.th.next_vid()
+
+    def click_prev_btn(self):
+        self.th.next_vid()
+
 
 class ViewDBCutVideos(OCVVideoThread):
 
-    def __init__(self, all_videos_df_path):
-        vid_df = pd.read_csv(all_videos_df_path)
+    def __init__(self, all_videos_df_path, db_path):
+        super().__init__()
+        self.all_videos = pd.read_csv(all_videos_df_path)
+        self.all_videos_path = sorted(list(self.all_videos.folder_name.unique()))
+        self.curr_video_idx = 0
+        self.cap = None
+        self.db_path = db_path
+        self.curr_frame_time_sec = None
+        self.curr_frame_time_ms = None
+        self.all_samples_path_from_video = None
+        self.curr_sample_idx = 0
+        self.curr_sample_opts = None
+        self.__update_videos_n_paths()
 
     def read_cur_video(self):
-        pass
+        if self.cap is None:
+            video_path = os.path.join(self.db_path, self.all_videos_path[self.curr_video_idx])
+            self.cap = cv2.VideoCapture(video_path)
+            self.curr_frame_time_sec = (1000 / self.cap.get(cv2.CAP_PROP_FPS)) / 1000
+            self.curr_frame_time_ms = 1000 / self.cap.get(cv2.CAP_PROP_FPS)
+
+            # pos 4: video name; pos 5: sinal;  pos 7: beg; pos 9: end + '.csv'
+            sample_name = \
+                self.all_samples_path_from_video[self.curr_sample_idx].replace('\\', '/').split('/')[-1].split('-')
+            self.curr_sample_opts = {'beg': float(sample_name[7]) - 500,
+                                     'end': float(sample_name[9].split('.')[0]) + 500,
+                                     'video_name': sample_name[5], 'sign': sample_name[4]}
+            succ = self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.curr_sample_opts['beg'] // self.curr_frame_time_ms)
+            if not succ:
+                raise RuntimeError(f'Could not set frame pos {video_path}')
+
+        time.sleep(self.curr_frame_time_sec)
+        ret, frame = self.cap.read()
+        if self.cap.get(cv2.CAP_PROP_POS_FRAMES) >= (self.curr_sample_opts['end'] // self.curr_frame_time_ms):
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.curr_sample_opts['beg'] // self.curr_frame_time_ms)
+
+        if not ret:
+            raise RuntimeError(f'bad video {self.all_videos_path[self.curr_video_idx]}')
+
+        self.__write_info_2_im(frame)
+        return ret, frame
+
+    def prev_sample(self):
+        self.curr_sample_idx = self.curr_sample_idx - 1 if self.curr_sample_idx != 0 \
+                                                        else len(self.all_samples_path_from_video)
+
+    def next_sample(self):
+        self.curr_sample_idx = self.curr_sample_idx + 1 \
+            if self.curr_sample_idx < len(self.all_samples_path_from_video) - 1 else 0
 
     def prev_vid(self):
-        pass
+        self.curr_video_idx = self.curr_video_idx - 1 if self.curr_video_idx != 0 else len(self.all_videos_path) - 1
+        self.__update_videos_n_paths()
 
     def next_vid(self):
-        pass
+        self.curr_video_idx = self.curr_video_idx + 1 if self.curr_video_idx < len(self.all_videos_path) - 1 else 0
+        self.__update_videos_n_paths()
+
+    def __update_videos_n_paths(self):
+        path_end_in_folder = self.all_videos_path[self.curr_video_idx].replace('\\', '/').split('/')[:-1]
+        path_end_in_folder = os.path.join(*path_end_in_folder)
+        video_path = os.path.join(self.db_path, path_end_in_folder)
+        self.all_samples_path_from_video = list(filter(lambda x: 'sample' in x, os.listdir(video_path)))
+        if self.cap is not None:
+            self.cap.release()
+
+
+    @staticmethod
+    def __convert_msec_2_hour_text(ms):
+        ms = int(ms)
+        seconds = (ms / 1000) % 60
+        seconds = int(seconds)
+        minutes = (ms/ (1000 * 60)) % 60
+        minutes = int(minutes)
+        hours = (ms / (1000 * 60 * 60)) % 24
+        return '%d: %d: %d' % (hours, minutes, seconds)
+
+    def __write_info_2_im(self, im):
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        bottom_left_corner_of_text = (10, 200)
+        font_scale = 0.3
+        font_color = (255, 255, 255)
+        line_type = 1
+
+        for it, opt in enumerate(self.curr_sample_opts.items()):
+            text = str(opt[1]) if not isinstance(opt[1], float) else self.__convert_msec_2_hour_text(opt[1])
+            cv2.putText(im, text,
+                        (bottom_left_corner_of_text[0], bottom_left_corner_of_text[1] + it * 10),
+                        font,
+                        font_scale,
+                        font_color,
+                        line_type)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    w = App()
+    w = App(th_cls=ViewDBCutVideos, all_videos_df_path='all_videos.csv',db_path='D:/gdrive/LibrasCorpus')
     w.show()
 
     sys.exit(app.exec_())
