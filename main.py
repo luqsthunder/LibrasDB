3e904f607c6f36d3c0dc49a712e3bce5f9046e35
Lucas <lafa@ic.ufal.br>
2020-11-19 11:23:16 -0300

    Conversão simples para 3D, apenas para testes.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from libras_classifiers.librasdb_loaders import DBLoader2NPY
 import pandas as pd
 import numpy as np
 import cv2 as cv
+from tqdm import tqdm
 
 left_front_data = pd.read_csv('v1098_barely_front_view_right_person.csv')
 left_side_data = pd.read_csv('v0180_side_view_right_person.csv')
@@ -58,24 +59,32 @@ if len(matches) >= 16:
     projection2[:3, :3] = R
     projection2[:, 3:] = t
 
-    for frame in all_frames:
+    dic = {
+        k: [] for k in left_front_data.keys()
+    }
+
+    for frame in tqdm(all_frames):
         row_front = left_front_data[left_front_data['frame'] == frame]
-        #row_front = row_front[all_joints_to_use].values[0]
-        row_front = [(x[1].values[0] if x[1].values[0][2] >= threshold else None, x[0]) 
-                     for x in row_front.iteritems() if not isinstance(x[0], float) and x[0] not in ['frame', 'Unnamed: 0']]
+        row_front = row_front.drop(columns=['frame', 'Unnamed: 0'])
+        row_front = row_front.applymap(lambda x: np.array([0.0, 0.0, 0.0]) if isinstance(x, float) else x)
+        row_front = [(x.values[0] if x.values[0][2] >= threshold else None, key)
+                     for key, x in row_front.iteritems()]
 
         row_side = left_side_data[left_side_data['frame'] == frame]
-        row_side = [(x[1].values[0] if x[1].values[0][2] >= threshold else None, x[0])
-                    for x in row_side.iteritems() if not isinstance(x[0], float) and x[0] not in ['frame', 'Unnamed: 0']]
+        row_side = row_side.drop(columns=['frame', 'Unnamed: 0'])
+        row_side = row_side.applymap(lambda x: np.array([0.0, 0.0, 0.0]) if isinstance(x, float) else x)
+        row_side = [(x.values[0] if x.values[0][2] >= threshold else None, key)
+                    for key, x in row_side.iteritems()]
 
         matches = []
+        dic['frame'].append(frame)
         for front, side in zip(row_front, row_side):
             if front[0] is not None and side[0] is not None:
                 #matches.append((front[0][:2], side[0][:2]))
                 points3d = cv.triangulatePoints(projection1, projection2,
-                                                np.array([front[0]]).reshape(len(matches), 1, 2),
-                                                np.array([side[0]]).reshape(len(matches), 1, 2))
-        
+                                                front[0][:2].reshape(1, 1, 2),
+                                                side[0][:2].reshape(1, 1, 2))
+                dic[front[1]].append(points3d)
 
 # cv::findEssentialMat (InputArray points1, InputArray points2, double focal=1.0, Point2d pp=Point2d(0, 0),
 #                       int method=RANSAC, double prob=0.999, double threshold=1.0, OutputArray mask=noArray())
