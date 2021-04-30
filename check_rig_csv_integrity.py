435f1edc0ee63cea187b50fb5216486bdd7df238
Lucas Amaral <lucas.amaral@edge.ufal.br>
2021-04-30 19:17:10 -0300

    f7

--- check_rig_csv_integrity.py
+++ check_rig_csv_integrity.py
@@ -12,7 +12,23 @@ csvs_with_zeros = []
 
 try:
     for db_folder in tqdm(all_db_folders):
-        for db_folder_csv in os.listdir(db_folder):
+        folder_content = os.listdir(db_folder)
+        files_csv = list(filter(os.path.isfile, files_csv))
+        more_folders = list(filter(os.path.isdir, files_csv))
+
+        if len(more_folders) > 0:
+            csvs_in_more = []
+            more_folders = list(map(lambda x: os.path.join(db_folder, x),
+                                    more_folders))
+            for folder_in_more in more_folders:
+                files_in_more = list(map(
+                    lambda x: os.path.join(x, folder_in_more),
+                    os.listdir(folder_in_more)
+                ))
+                csvs_in_more.extend(files_in_more)
+            files_csv.extend(csvs_in_more)
+
+        for db_folder_csv in files_csv:
             db_folder_csv = os.path.join(db_folder, db_folder_csv)
             sign_df = pd.read_csv(db_folder_csv)
             if sign_df.shape[0] == 0:
