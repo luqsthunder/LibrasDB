9253a0fcf08b296ad5820ebefc07a9155fb6a15d
Lucas Amaral <lucas.amaral@edge.ufal.br>
2021-04-30 18:46:24 -0300

    f6

--- check_rig_csv_integrity.py
+++ check_rig_csv_integrity.py
@@ -9,11 +9,8 @@ all_db_folders = list(map(lambda x: os.path.join(db_path, x), all_db_folders))
 csvs_with_zeros = []
 
 for db_folder in all_db_folders:
-    for db_folder_csvs in os.listdir(db_folder):
-        db_folder_csvs = list(map(lambda x: os.path.join(db_folder, x),
-                                  db_folder_csvs))
-        print(db_folder_csvs)
-        db_folder_csvs = list(map(pd.read_csv, db_folder_csvs))
-        if any(list(map(lambda x: x.shape[0] == 0, db_folder_csvs))):
-            print(db_folder_csvs)
-        #csvs_with_zeros.extend([x for x in db_folders_csvs if x.shape[0] == 0])
+    for db_folder_csv in os.listdir(db_folder):
+        db_folder_csv = os.path.join(db_folder, db_folder_csv)
+        sign_df = pd.read_csv(db_folder_csv)
+        if sign_df.shape[0] == 0:
+            print(db_folder_csv)
