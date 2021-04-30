41238d313e8a16229860fa4262e169d840aa7c80
Lucas Amaral <lucas.amaral@edge.ufal.br>
2021-04-30 18:53:06 -0300

    f7

--- check_rig_csv_integrity.py
+++ check_rig_csv_integrity.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import pandas as pd
+from tqdm import tqdm
 
 db_path = '../sign_db_front_view/sign_db_rig/'
 
@@ -8,9 +10,14 @@ all_db_folders = list(map(lambda x: os.path.join(db_path, x), all_db_folders))
 
 csvs_with_zeros = []
 
-for db_folder in all_db_folders:
-    for db_folder_csv in os.listdir(db_folder):
-        db_folder_csv = os.path.join(db_folder, db_folder_csv)
-        sign_df = pd.read_csv(db_folder_csv)
-        if sign_df.shape[0] == 0:
-            print(db_folder_csv)
+try:
+    for db_folder in tqdm(all_db_folders):
+        for db_folder_csv in os.listdir(db_folder):
+            db_folder_csv = os.path.join(db_folder, db_folder_csv)
+            sign_df = pd.read_csv(db_folder_csv)
+            if sign_df.shape[0] == 0:
+                print(db_folder_csv)
+except BaseException as e:
+    print(e)
+    sys.exit(-1)
+
